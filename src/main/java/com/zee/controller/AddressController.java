61fd7c12d2cd56df87bdbab8e8937ff70c8ecf2c
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-06-20 22:29:37 -0400

    Lab

--- src/main/java/com/zee/controller/AddressController.java
+++ src/main/java/com/zee/controller/AddressController.java
@@ -1,16 +1,15 @@
 package com.zee.controller;
 
+import com.zee.dto.AddressDTO;
 import com.zee.dto.ResponseWrapper;
+import com.zee.entity.Address;
 import com.zee.service.AddressService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/address/{addressNo}")
+@RequestMapping("/api/v1")
 public class AddressController {
 
     private final AddressService addressService;
@@ -30,18 +29,24 @@ public class AddressController {
      "code":200
      "data":<address data>
     */
-    @GetMapping
+    @GetMapping("/address/{addressNo}")
     public ResponseEntity<ResponseWrapper> retrieveAddress(@PathVariable("addressNo") String addressNo) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new ResponseWrapper("Address " + addressNo + "is successfully retrieved.",addressService.findByAddressNo(addressNo)));
+                .body(new ResponseWrapper("Address " + addressNo + "is successfully retrieved.", addressService.findByAddressNo(addressNo)));
 
     }
+
     /*
       Endpoint: /api/v1/address/{addressNo}
 
       JSON Response Body:
       <updated address data>
      */
+    @PutMapping("/address/{addressNo}")
+    public ResponseEntity<ResponseWrapper> updateAddressNo(@PathVariable("addressNo") String addressNo, @RequestBody AddressDTO address) {
 
+        return ResponseEntity.ok(new ResponseWrapper("successfully updated", addressService.update(addressNo, address)));
+
+    }
 }
